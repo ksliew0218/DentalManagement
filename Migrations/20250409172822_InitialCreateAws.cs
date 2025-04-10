052005ea4ec81ce0ca1fdae3a596982507656da0
Samuel Liu <liuhanpinsamuel030331@gmail.com>
2025-04-10 12:59:21 +0800

    AWS RDS Database setting

---
+++ Migrations/20250409172822_InitialCreateAws.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DentalManagement.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreateAws : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ Migrations/20250410042556_AddColumnsToDoctorTreatment.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DentalManagement.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddColumnsToDoctorTreatment : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "DoctorTreatments",
+                nullable: false,
+                defaultValue: DateTime.UtcNow);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "DoctorTreatments",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "DoctorTreatments",
+                nullable: false,
+                defaultValue: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "DoctorTreatments",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "DoctorTreatments");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "DoctorTreatments");
+
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "DoctorTreatments");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "DoctorTreatments");
+        }
+    }
+}
